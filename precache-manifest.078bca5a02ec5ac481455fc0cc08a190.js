3e2e9f344b9070077b344a1076d2f7f55c74fb8f
Prabu <prabube.mani@gmail.com>
2019-09-06 14:09:34 +0530

    Updates

---
+++ precache-manifest.078bca5a02ec5ac481455fc0cc08a190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c1133ca2a2ad10fb809406e5e1c6851",
+    "url": "/reacttestapp/index.html"
+  },
+  {
+    "revision": "fd4b174cfd2dd09e86aa",
+    "url": "/reacttestapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e524043847c8f3b91c48",
+    "url": "/reacttestapp/static/js/2.cc9ac622.chunk.js"
+  },
+  {
+    "revision": "fd4b174cfd2dd09e86aa",
+    "url": "/reacttestapp/static/js/main.d5b27c2f.chunk.js"
+  },
+  {
+    "revision": "f2dde15c96dc0d4a517b",
+    "url": "/reacttestapp/static/js/runtime~main.8f9ff2aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reacttestapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
